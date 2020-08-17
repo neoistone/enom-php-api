79d5ef60daef20e92933895875437aaf9185e80d
Neoistone india <59179052+neoistone@users.noreply.github.com>
2020-08-18 03:47:40 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,10 +3,10 @@ include __DIR__.'/src/enom.php';
 
 // live api
 $enom = new neoistone\Enom\api('username','password','response type json or xml');
-  $enom->command(array(
+  $enom->command'check',(array(
   	'sld' => 'neoistone',
   	'tld' => 'org',
-  ),'check');
+  ));
   echo $enom->run();
 
 //test api
@@ -16,6 +16,6 @@ $enom = new neoistone\Enom\api('username','password','response type json or xml'
   $enom->command('check',array(
   	'sld' => 'neoistone',
   	'tld' => 'org',
-  ),);
+  ));
   echo $enom->run();
 ?>
